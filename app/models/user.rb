354b43295c35bab28e056dc07924b67a2bf130b5
tanaka_koji <starmelt190@gmail.com>
2020-04-25 22:53:28 +0900

    micropostのアイコンのcssデザイン軽度な修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ApplicationRecord
   validates :realname,presence: true, unless: :uid?,length: { maximum: 30 }
   validates :name,    presence: true, unless: :uid?,length: { maximum: 30 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email,presence: true, unless: :uid?,length: { maximum: 255 },
-            format: { with:VALID_EMAIL_REGEX },
-            uniqueness: { case_sensitive: false}
+  #validates :email,presence: true, unless: :uid?,length: { maximum: 255 },
+            #format: { with:VALID_EMAIL_REGEX },
+            #uniqueness: { case_sensitive: false}
   has_secure_password validations: false
   validates :password,presence: true,length: { minimum: 6 }, confirmation: true, unless: :uid?,
   allow_nil: true
